728d4c93d28556bd4ca884cc46bc2c06a3e8731d
saketh.saripalli <saketh.saripalli@gmail.com>
2021-03-16 11:18:30 -0400

    Added english and spanish

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ my_bot = ChatBot(name='Mysterybot', read_only=True)
 
 #Trained with given french corpus
 corpus_trainer = ChatterBotCorpusTrainer(my_bot)
-corpus_trainer.train('chatterbot.corpus.french')
+corpus_trainer.train('chatterbot.corpus.english')
 
 
 @app.route("/")
@@ -26,6 +26,27 @@ def home():
 def get_bot_response():
     userText = request.args.get('msg')
     return str(my_bot.get_response(userText))
+
+@app.route("/setenglish")
+def set_english_response():
+    my_bot = ChatBot(name='Mysterybot', read_only=True)
+    corpus_trainer = ChatterBotCorpusTrainer(my_bot)
+    corpus_trainer.train('chatterbot.corpus.english')
+    return 'Hi I am Mark Jackson!'
+
+@app.route("/setfrench")
+def set_french_response():
+    my_bot = ChatBot(name='Mysterybot', read_only=True)
+    corpus_trainer = ChatterBotCorpusTrainer(my_bot)
+    corpus_trainer.train('chatterbot.corpus.french')
+    return 'Hi I am Jean Claude!'
+
+@app.route("/setspanish")
+def set_spanish_response():
+    my_bot = ChatBot(name='Mysterybot', read_only=True)
+    corpus_trainer = ChatterBotCorpusTrainer(my_bot)
+    corpus_trainer.train('chatterbot.corpus.spanish')
+    return 'Hi I am Estabon Zaragoza!'
 
 
 if __name__ == "__main__":
